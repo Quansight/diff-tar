2f570b8036421def24130a34f1f4233a2a2593c2
Ilan Schnell <ilanschnell@gmail.com>
2020-01-22 22:36:21 -0600

    add readme and Python code

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# diff-tar
-create differential tarball of conda repository
+Create differential tarballs
+============================
+
+Note: this project is still experimental (tested against Python 3.8)
+
+This tools allows you to create differential tarballs of a (usually
+mirrored) conda repository.  The resulting tarball can be used to update
+a copy of the mirror on a remote (air-gapped) system, without having to
+copy the entire conda repository.  The workflow is a follows:
+
+  1. we assume that the remote and local repository are in sync
+  2. create a `reference.json` file of the local repository
+  3. update the local repository using `conda-mirror` or some other tools
+  4. create the "differential" tarball
+  5. move the differential tarball to the remote machine, and unpack it
+  6. now that the remote repository is up-to-date, we should create a new
+     `reference.json` on the local machine.  That is, step 2
+
+Notes:
+------
+
+The file `reference.json` is a collection of all `repodata.json`
+files (`linux-64`, `win-32`, `noarch`, etc.) of the local repository.
+It is created in order to compare a future state of the repository to the
+state of the repository when `reference.json` it was created.
+
+The differential tarball contains files which either have been updated (such
+as `repodata.json`) or new files (new conda packages).  It is meant to be
+unpacked on top of the existing mirror on the remote machine by:
+
+    cd <repository>
+    tar xf update.tar
+    # or y using tar's -C option from any directory
+    tar xf update.tar -C <repository>
+
+
+Example:
+--------
+
+In this example we assume that a conda mirror is located in `./repo`.
+Create `reference.json`:
+
+    python mk_diff_tar.py --reference ./repo
+
+Show the files in respect to the latest reference point file (which would be
+included in the differential tarball).  Since we just created the reference
+file, we don't expect any output:
+
+    python mk_diff_tar.py --show ./repo
+
+Now, we can update the mirror:
+
+    conda-mirror --upstream-channel conda-forge --target-directory ./repo ...
+
+Create the actual differential tarball:
+
+    $ python mk_diff_tar.py --create ./repo
+    Wrote: update.tar
+    $ tar tf update.tar
+    noarch/repodata.json
+    noarch/repodata.json.bz2
+    noarch/ablog-0.9.2-py_0.tar.bz2
+    noarch/aws-amicleaner-0.2.2-py_0.tar.bz2
+    ...
---
+++ mk_diff_tar.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import json
+import hashlib
+import tarfile
+from os.path import abspath, isdir, join, relpath
+
+
+__version__ = '1.0.0'
+verbose = False
+mirror_dir = None
+
+
+def md5_file(path):
+    h = hashlib.new('md5')
+    with open(path, 'rb') as fi:
+        while 1:
+            chunk = fi.read(262144)
+            if not chunk:
+                break
+            h.update(chunk)
+    return h.hexdigest()
+
+def find_repos():
+    for root, unused_dirs, files in os.walk(mirror_dir):
+        if 'repodata.json' in files and 'repodata.json.bz2' in files:
+            yield root
+
+def all_repodata():
+    d = {}
+    for repo_path in find_repos():
+        with open(join(repo_path, 'repodata.json')) as fi:
+            index = json.load(fi)['packages']
+        d[repo_path] = index
+    return d
+
+def verify_all_repos():
+    d = all_repodata()
+    for repo_path, index in d.items():
+        for fn, info in index.items():
+            path = join(repo_path, fn)
+            if info['md5'] == md5_file(path):
+                continue
+            print('MD5 mismatch: %s' % path)
+
+def write_reference(path):
+    data = json.dumps(all_repodata(), indent=2, sort_keys=True)
+    # make sure we have newline at the end
+    if not data.endswith('\n'):
+        data += '\n'
+    with open(path, 'w') as fo:
+        fo.write(data)
+
+def get_updates():
+    with open('reference.json') as fi:
+        d1 = json.load(fi)
+    d2 = all_repodata()
+    result = []
+    for repo_path, index2 in d2.items():
+        index1 = d1.get(repo_path, {})
+        if index1 != index2:
+            for fn in 'repodata.json', 'repodata.json.bz2':
+                result.append(relpath(join(repo_path, fn), mirror_dir))
+        for fn, info2 in index2.items():
+            info1 = index1.get(fn, {})
+            if info1.get('md5') != info2['md5']:
+                result.append(relpath(join(repo_path, fn), mirror_dir))
+    return result
+
+def tar_repo():
+    fn = 'update.tar'
+    t = tarfile.open(fn, 'w')
+    for f in get_updates():
+        if verbose:
+            print('adding: %s' % f)
+        t.add(join(mirror_dir, f), f)
+    t.close()
+    print("Wrote: %s" % fn)
+
+def main():
+    from optparse import OptionParser
+
+    p = OptionParser(usage="usage: %prog [options] MIRROR_DIRECTORY",
+                     description='create "differential" tarballs of a conda '
+                                 'mirror repository')
+
+    p.add_option('--create',
+                 action="store_true",
+                 help="create a differential tarball")
+
+    p.add_option('--reference',
+                 action="store_true",
+                 help="create a reference point file and exit")
+
+    p.add_option('--show',
+                 action="store_true",
+                 help="show the files in respect to the latest reference "
+                      "point file (which would be included in the "
+                      "differential tarball) and exit")
+
+    p.add_option('--verify',
+                 action="store_true",
+                 help="verify the mirror repository and exit")
+
+    p.add_option('-v', '--verbose',
+                 action="store_true")
+
+    p.add_option('--version',
+                 action="store_true",
+                 help="print version and exit")
+
+    opts, args = p.parse_args()
+
+    if len(args) != 1:
+        p.error('exactly one argument is required, try -h')
+
+    global mirror_dir
+    mirror_dir = abspath(args[0])
+    if not isdir(mirror_dir):
+        sys.exit("no such directory: %r" % mirror_dir)
+
+    if opts.verbose:
+        global verbose
+        verbose = True
+
+    if opts.version:
+        print(__version__)
+        return
+
+    if opts.create:
+        tar_repo()
+        return
+
+    if opts.verify:
+        verify_all_repos()
+        return
+
+    if opts.show:
+        for path in get_updates():
+            print(path)
+        return
+
+    if opts.reference:
+        write_reference('reference.json')
+        return
+
+    print("Nothing done.")
+
+if __name__ == '__main__':
+    main()
